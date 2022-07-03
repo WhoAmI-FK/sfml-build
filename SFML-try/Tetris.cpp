6b68f15b49334c7786373925e7b7239a0e22c8e5
WhoAmI-FK <Faismen2004@mail.ru>
2022-07-03 15:24:35 +0200

    tetris, fixes require

--- SFML-try/Tetris.cpp
+++ SFML-try/Tetris.cpp
@@ -1,5 +1,5 @@
 #include <SFML/Graphics.hpp>
-
+#include <time.h>
 const int M = 20;
 const int N = 10;
 
@@ -19,18 +19,34 @@ int figures[7][4] =
 struct Point
 {
 	int x, y;
-}a[4],b[4];
+}a[4], b[4];
+
+bool check()
+{
+	for (int i = 0; i < 4; i++)
+		if (a[i].x < 0 || a[i].x >= N || a[i].y >= M) return false;
+		else if (field[a[i].y][a[i].x]) return false;
+
+	return true;
+}
+
 
 
 int main()
 {
+	srand(time(0));
 	sf::RenderWindow window(sf::VideoMode(320, 480), "Tetris");
-	sf::Texture texture;
+	sf::Texture texture,texture_background,texture_frame;
 	texture.loadFromFile("C:\\Users\\User\\source\\repos\\sfml-try\\SFML-try\\images-tetris\\tiles.png");
-	sf::Sprite sprite(texture);
+	texture_background.loadFromFile("C:\\Users\\User\\source\\repos\\sfml-try\\SFML-try\\images-tetris\\background.png");
+	texture_frame.loadFromFile("C:\\Users\\User\\source\\repos\\sfml-try\\SFML-try\\images-tetris\\frame.png");
+	sf::Sprite sprite(texture), sprite_background(texture_background), sprite_frame(texture_frame);
 	sprite.setTextureRect(sf::IntRect(0, 0, 18, 18));
 	int dx = 0;
 	bool rotate = false;
+	int colorNum = 1;
+	bool beginGame = true;
+	int n = rand() % 7;
 	float timer = 0, delay = 0.3;
 	sf::Clock _clock;
 	while(window.isOpen())
@@ -50,7 +66,20 @@ int main()
 				else if (_ev.key.code == sf::Keyboard::Left) dx = -1;
 				else if (_ev.key.code == sf::Keyboard::Right) dx = 1;
 			}
-			for (int i = 0; i < 4; i++) a[i].x += dx;
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) delay = 0.05;
+			
+			for (int i = 0; i < 4; i++)
+			{
+				b[i] = a[i];
+				a[i].x += dx;
+			}
+			
+			if (!check())
+			{
+				for (int i = 0; i < 4; i++)
+					a[i] = b[i];
+			}
 
 			if (rotate)
 			{
@@ -62,30 +91,83 @@ int main()
 					a[i].x = p.x - x;
 					a[i].y = p.y + y;
 				}
+				if (!check()) {
+					for (int i = 0; i < 4; i++)
+					{
+						a[i] = b[i];
+					}
+				}
 			}
 			if (timer > delay)
 			{
-				for (int i = 0; i < 4; i++) a[i].y += 1;
+				for (int i = 0; i < 4; i++)
+				{
+					b[i] = a[i];
+					a[i].y += 1;
+				}
+				if (!check())
+				{
+					for (int i = 0; i < 4; i++) {
+						field[b[i].y][b[i].x] = colorNum;
+					}
+					colorNum = 1 + rand() % 7;
+					int n = rand() % 7;
+					for (int i = 0; i < 4; i++)
+					{
+						a[i].x = figures[n][i] % 2;
+						a[i].y = figures[n][i] / 2;
+					}
+				}
 				timer = 0;
 			}
 		}
-		int n = 3;
-		if (a[0].x == 0) {
+
+		int k = M - 1;
+		for (int i = M - 1; i > 0; i--)
+		{
+			int count = 0;
+			for (int j = 0; j < N; j++)
+			{
+				if (field[i][j]) count++;
+				field[k][j] = field[i][j];
+			}
+			if (count < N) k--;
+		}
+
+		if (beginGame)
+		{
+			beginGame = false;
+			n = rand() % 7;
 			for (int i = 0; i < 4; i++)
 			{
 				a[i].x = figures[n][i] % 2;
 				a[i].y = figures[n][i] / 2;
 			}
 		}
+
 		dx = 0;
 		rotate = false;
 
 		window.clear(sf::Color::White);
+		window.draw(sprite_background);
+		for (int i = 0; i < M; i++)
+		{
+			for (int j = 0; j < N; j++) {
+				if (field[i][j] == 0) continue;
+				sprite.setTextureRect(sf::IntRect(field[i][j] * 18, 0, 18, 18));
+				sprite.setPosition(j * 18, i * 18);
+				sprite.move(28, 31);
+				window.draw(sprite);
+			}
+		}
 		for (int i = 0; i < 4; i++)
 		{
+			sprite.setTextureRect(sf::IntRect(colorNum * 18, 0, 18, 18));
 			sprite.setPosition(a[i].x * 18, a[i].y * 18);
+			sprite.move(28, 31);
 			window.draw(sprite);
 		}
+		window.draw(sprite_frame);
 		window.display();
 	}
 

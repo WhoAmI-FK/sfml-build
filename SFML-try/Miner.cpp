949722120062315118fb7a9409e6cab9b9f0835e
WhoAmI-FK <Faismen2004@mail.ru>
2022-07-12 20:27:31 +0200

    sound effects added

--- SFML-try/Miner.cpp
+++ SFML-try/Miner.cpp
@@ -1,6 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <utility>
 #include <ctime>
 
+
+/* Required fixes
+*  1. add new features
+*  2. Code Refactor
+*/
+
+typedef sf::SoundBuffer sfSBuffer;
+typedef sf::Sound sfSound;
 typedef sf::Texture sfTexture;
 typedef sf::Sprite sfSprite;
 typedef sf::Event sfEvent;
@@ -14,7 +24,20 @@ int main()
 	int gridView[12][12];
 	bool selected = false;
 	bool end_game = false;
-	// add sound
+	sfSound sPlayer;
+	// soundefx
+	std::pair <sfSBuffer, std::shared_ptr<sfSound>> gameBegin;
+	std::pair <sfSBuffer, std::shared_ptr<sfSound>> fieldClicked;
+	std::pair <sfSBuffer, std::shared_ptr<sfSound>> bombExplosion;
+	bombExplosion.first.loadFromFile("sounds-minesweeper\\ogg\\explosion.ogg");
+	bombExplosion.second = std::make_shared<sfSound>(sPlayer);
+	fieldClicked.first.loadFromFile("sounds-minesweeper\\ogg\\fieldclicked.ogg");
+	fieldClicked.second = std::make_shared<sfSound>(sPlayer);
+	gameBegin.first.loadFromFile("sounds-minesweeper\\ogg\\game-start.ogg");
+	gameBegin.second = std::make_shared<sfSound>(sPlayer);
+	gameBegin.second.get()->setBuffer(gameBegin.first);
+	gameBegin.second.get()->play();
+	// soundefx
 	int mines_found = 0;
 	int mines = 0;
 
@@ -66,11 +89,24 @@ int main()
 
 			if (e.type == sfEvent::MouseButtonPressed)
 			{
-				if (e.key.code == sf::Mouse::Left) gridView[x][y] = gridLogic[x][y];
+				fieldClicked.second.get()->setBuffer(fieldClicked.first);
+				fieldClicked.second.get()->play();
+				if (e.key.code == sf::Mouse::Left) {
+					gridView[x][y] = gridLogic[x][y];
+					selected = true;
+				}
 				else if (e.key.code == sf::Mouse::Right) gridView[x][y] = 11;
 			}
 		}
-	
+		if (selected == true)
+		{
+			if (e.key.code == sf::Mouse::Left && gridLogic[x][y] == 9)
+			{
+				bombExplosion.second.get()->setBuffer(bombExplosion.first);
+				bombExplosion.second.get()->play();
+			}
+		}
+
 		app.clear(sfColor::White);
 		for(int i = 1;i<=10;i++)
 			for (int j = 1; j <= 10; j++)
@@ -80,6 +116,8 @@ int main()
 				s.setPosition(i * w, j * w);
 				app.draw(s);
 			}
+
+		selected = false;
 		app.display();
 	}
 
